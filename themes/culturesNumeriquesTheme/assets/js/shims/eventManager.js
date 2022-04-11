d53d980a8a249e79a87f4b2e288b3919ca7c2e83
Arthaud Proust <arthaudp33@gmail.com>
2022-04-11 16:46:57 +0200

    add event manager js

--- themes/culturesNumeriquesTheme/assets/js/shims/eventManager.js
+++ themes/culturesNumeriquesTheme/assets/js/shims/eventManager.js
@@ -1,23 +1,193 @@
 class EventManager {
-    constructor(rootElement) {
-        this.rootEl = rootElement;
-        this.fetchedEvents = [];
+    constructor(rootElementSelector) {
+        this.rootEl = document.querySelector(rootElementSelector);
+
+        this.events = {
+            fetched: [],
+            filtered: []
+        }
+        
+        this.eventTemplate = document.querySelector("template#event-template");
+        
+        this.pagination = {
+            enabled: true,
+            countPerPage: 9,
+            actualPage: 0
+        };
+
+        return this;
     }
 
-    fetchAll() {
+    /**
+     * Retourne l'élément html template
+     * 
+     * @returns {DOMElement} Template évènement 
+     */
 
+    getTemplateEl() {
+        return document.importNode(this.eventTemplate.content, true)
+    }
+
+    /**
+     * Récupère tous les évènements
+     * 
+     * @returns {Promise(EventManager)} Promise
+     */
+    async fetchAll() {
+        let blob = await fetch('/events/index.json');
+        let json = await blob.json();
+        this.events.fetched = json.data;
+        this.clearFilter();
+        return this;
+    }
+
+    /**
+     * Remets dans la liste tous les évènements
+     * 
+     * @returns {EventManager} EventManager
+     */
+    clearFilter() {
+        this.events.filtered = this.events.fetched;
+
+        return this;
+    }
+
+    /**
+     * Filtre les évènements selon l'année donnée
+     * 
+     * @param {string} year
+     * @param {string} filterFetched - Si true filtre la liste events.fetched
+     * 
+     * @returns {EventManager} EventManager
+     */
+    filterByYear(year, filterFetched=true) {
+        const eventsToFilter = filterFetched ? [...this.events.fetched] : [...this.events.filtered];
+        this.events.filtered = [];
+
+        for(let event of eventsToFilter) {
+            if(event.year == year) {
+                this.events.filtered.push(event);
+            }
+        }
+
+        return this;
+    }
+
+    /**
+     * Filtre les évènements selon la thématique donnée
+     * 
+     * @param {string} thematique
+     * @param {string} filterFetched - Si true filtre la liste events.fetched
+     * 
+     * @returns {EventManager} EventManager
+     */
+    filterByThematique(thematique, filterFetched) {
+        const eventsToFilter = filterFetched ? [...this.events.fetched] : [...this.events.filtered];
+        this.events.filtered = [];
+
+        for(let event of eventsToFilter) {
+            if(event.thematique == thematique) {
+                this.events.filtered.push(event);
+            }
+        }
+        return this;
     }
 
     orderBy() {
 
+        return this;
+    }
+
+    /**
+     * Vide le DOM de l'élément root
+     * 
+     * @returns {EventManager} EventManager
+     */
+    clear() {
+        while (this.rootEl.lastElementChild) {
+            this.rootEl.removeChild(this.rootEl.lastElementChild);
+        }
+
+        return this;
+    }
+
+    /**
+     * Ajoute à l'élément root un élément event rempli avec les données fournies
+     * 
+     * @param {object} eventData
+     * 
+     * @returns {EventManager} EventManager
+     */
+    renderEvent(eventData) {
+        let el = this.getTemplateEl();
+        el.querySelector('.event-summary-title').innerText = eventData.title;
+        el.querySelector('.event-summary-abstract').innerText = eventData.abstract;
+        el.querySelector('.event-summary-image').src = eventData.image;
+        el.querySelector('.event-summary-permalink').href = eventData.permalink;
+        this.rootEl.append(el);
+
+        return this;
+    }
+
+    /**
+     * Actualise le DOM de l'élément root
+     * 
+     * @returns {EventManager} EventManager
+     */
+    render() {
+        let iStart, iMax;
+        if(!this.pagination.enabled) {
+            iStart = 0;
+            iMax = this.events.filtered.length;
+        } else {
+            iStart = this.pagination.actualPage * this.pagination.countPerPage;
+            iMax = Math.min(iStart+this.pagination.countPerPage, this.events.filtered.length);
+        }
+        for(let i=iStart; i<iMax; i++) {
+            this.renderEvent(this.events.filtered[i])
+        }
+
+        return this;
     }
 
 
     /**
-     * Actualise le DOM de l'élément parent
+     * Affiche les résultats de la page précédente si on est pas au début
+     * 
+     * @returns {EventManager} EventManager
      */
-    render() {
+    paginatePrevious() {
+        if(this.pagination.actualPage>0) {
+            this.pagination.actualPage--;
+            this.clear().render();
+        }
 
+        return this;
+    }
+
+    /**
+     * Affiche les résultats de la page suivante page suivante
+     * 
+     * @returns {EventManager} EventManager
+     */
+    paginateNext() {
+        this.pagination.actualPage++;
+        this.clear().render();
+
+        return this;
+    }
+
+
+    /**
+     * Affiche les résultats de la page suivante page suivante sans effacer le reste
+     * 
+     * @returns {EventManager} EventManager
+     */
+    loadMore() {
+        this.pagination.actualPage++;
+        this.render();
+
+        return this;
     }
 }
 
