7b0f974c6a0be0d71dba72786a8a5d9a8415ee74
Arthaud Proust <arthaudp33@gmail.com>
2022-04-12 20:05:38 +0200

    header

--- themes/culturesNumeriquesTheme/assets/js/app.js
+++ themes/culturesNumeriquesTheme/assets/js/app.js
@@ -1,82 +1,10 @@
-import { EventManager } from "./shims/eventManager";
+window.addEventListener('DOMContentLoaded', function() {
 
-const eventManager = new EventManager('#events-homepage');
-
-const paginationActualText = document.getElementById("eventManager-paginationActual");
-const paginationPreviousBtn = document.getElementById("eventManager-paginationPrevious");
-const paginationNextBtn = document.getElementById("eventManager-paginationNext");
-const eventTypeFilters = document.querySelectorAll("#eventManager-type-filters input");
-
-
-eventManager.fetchAll().then(()=>{
-    // eventManager.renderDOM();
-    eventManager
-        // .addFilter('year', 2000)
-        .addFilter('kind', 'inventions')
-        .filter();
-    console.log(eventManager.pagination);
-    eventManager
-        .createPagination()
-        .clearDOM()
-        .renderDOM();
-
-    updatePagination();
-
-    return;
+    console.log('e');
+    const menu = document.getElementById('menu');
+    const openMenu = document.getElementById('openMenu');
+    const closeMenu = document.getElementById('closeMenu');
     
-
-    eventTypeFilters.forEach(filterCheckbox=>filterCheckbox.addEventListener('change', e=>{
-        if(e.target.checked) {
-            eventManager.addFilter("type", e.target.dataset.type);
-        } else {
-            eventManager.removeFilter("type", e.target.dataset.type);
-        }
-    }))
-    // document.getElementById('eventManager-loadMore').addEventListener('click', ()=>eventManager.loadMore());
-
-    paginationPreviousBtn.addEventListener('click', ()=>{
-        eventManager.paginatePrevious();
-        updatePagination();
-    })
-
-    paginationNextBtn.addEventListener('click', ()=>{
-        eventManager.paginateNext();
-        updatePagination();
-    })
-});
-
-
-
-
-
-
-function getPageDates(pageN) {
-    const start = eventManager.pagination.pages[pageN][0].year,
-        end = eventManager.pagination.pages[pageN][eventManager.pagination.pages[pageN].length-1].year;
-    return {
-        start,
-        end, 
-        readable: `${start}-${end}`
-    }
-}
-
-function updatePagination() {
-    currPageDates = getPageDates(eventManager.pagination.currentPageN);
-
-    if(eventManager.canPaginateNext()) {
-        paginationNextBtn.classList.remove('disabled');
-        paginationNextBtn.innerText = getPageDates(eventManager.pagination.currentPageN+1).readable
-    } else {
-        paginationNextBtn.classList.add('disabled');
-    }
-
-
-    if(eventManager.canPaginatePrevious()) {
-        paginationPreviousBtn.classList.remove('disabled');
-        paginationPreviousBtn.innerText = getPageDates(eventManager.pagination.currentPageN-1).readable
-    } else {
-        paginationPreviousBtn.classList.add('disabled');
-    }
-
-    paginationActualText.innerText = currPageDates.readable;
-}
+    openMenu.addEventListener('click', e=>menu.classList.remove('d-none'))
+    closeMenu.addEventListener('click', e=>menu.classList.add('d-none'))
+})
