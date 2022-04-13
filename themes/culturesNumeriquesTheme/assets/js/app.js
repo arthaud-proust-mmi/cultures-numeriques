2340e12d9674f25f484a278b686b85d4337eddc9
Arthaud Proust <arthaudp33@gmail.com>
2022-04-13 10:47:29 +0200

    headeuuur

--- themes/culturesNumeriquesTheme/assets/js/app.js
+++ themes/culturesNumeriquesTheme/assets/js/app.js
@@ -1,10 +1,1 @@
-window.addEventListener('DOMContentLoaded', function() {
-
-    console.log('e');
-    const menu = document.getElementById('menu');
-    const openMenu = document.getElementById('openMenu');
-    const closeMenu = document.getElementById('closeMenu');
-    
-    openMenu.addEventListener('click', e=>menu.classList.remove('d-none'))
-    closeMenu.addEventListener('click', e=>menu.classList.add('d-none'))
-})
+import "./bootstrap";
