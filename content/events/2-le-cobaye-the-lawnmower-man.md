16a52f5b9503e6593b1ad6873676e72cfb163296
Arthaud Proust <arthaudp33@gmail.com>
2022-04-14 10:16:00 +0200

    try fixing masonry lazyload

--- content/events/2-le-cobaye-the-lawnmower-man.md
+++ content/events/2-le-cobaye-the-lawnmower-man.md
@@ -10,5 +10,4 @@ abstract: Un homme simple d'esprit fait l'objet d'une expérience scientifique
   extraordinaire, jusqu'à lui conférer des pouvoirs parapsychologiques, mais
   dans le même temps son équilibre mental est de plus en plus perturbé.
 image: /images/events/18455828.jpg-r_1280_720-f_jpg-q_x-xxyxx.jpg
-gallery: /images/events/capture-d-ecran-2022-03-22-124857.jpg
 ---
--- themes/culturesNumeriquesTheme/assets/js/app.js
+++ themes/culturesNumeriquesTheme/assets/js/app.js
@@ -1,2 +1,22 @@
 import "./bootstrap.js";
 import "masonry-layout";
+import imagesLoaded from "imagesloaded";
+
+
+
+var grids = document.querySelectorAll('.grid-masonry');
+
+
+
+grids.forEach(grid=>{
+    var msnry = new Masonry( grid, {
+        // itemSelector: '.grid-item',
+        // columnWidth: '.grid-sizer',
+        percentPosition: true
+    });
+
+    imagesLoaded( grid ).on( 'progress', function() {
+        // layout Masonry after each image loads
+        msnry.layout();
+    });
+})
